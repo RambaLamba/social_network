2efb685add3b1a5ecb770116a6a75bb5f300ff7c
Alexander <rambalamba21@gmail.com>
2026-02-01 22:20:12 +0300

    Update settings.py

--- web_test/settings.py
+++ web_test/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1izllvfv7icupxhq8y24!2mj#=qc=qn*7jm=17&p2t(@@mbt*r'
+SECRET_KEY = 'secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -120,5 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
